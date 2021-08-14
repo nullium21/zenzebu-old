5fc8c506423fc02f166192ae620d747d3397c3c9
nullium21 <rommyskovets@gmail.com>
2021-08-14 12:22:13 +0300

    add a ecs 'subsystem'

---
+++ zenzebu/src/zz/ecs.cc
@@ -0,0 +1,29 @@
+#include "zz/ecs.h"
+#include "zz/log.h"
+
+using namespace zz::ecs;
+
+shared_ptr<registry> ecs::entt_reg;
+
+void ecs::init() {
+    ZZ_CORE_INFO("ECS initializing...");
+}
+
+entity ecs::new_entity() {
+    return entt_reg->create();
+}
+
+template<typename T, typename... Args>
+T& ecs::component(entity e, Args&&... args) {
+    return entt_reg->emplace<T>(e, std::forward<Args>(args)...);
+}
+
+template<typename T>
+T& ecs::get(entity e) {
+    return entt_reg->get<T>(e);
+}
+
+template<typename... T>
+void ecs::remove(entity e) {
+    entt_reg->remove<T...>(e);
+}
--- zenzebu/src/zz/ecs.h
+++ zenzebu/src/zz/ecs.h
@@ -1,1 +1,32 @@
+#pragma once
+
 #include "entt/entt.hpp"
+
+#include <memory>
+
+using std::shared_ptr;
+
+namespace zz::ecs {
+
+    using entt::registry;
+    using entt::entity;
+
+    class ecs {
+    public:
+        static void init();
+        inline static shared_ptr<registry> &entt_registry() { return entt_reg; }
+
+        static entity new_entity();
+
+        template<typename T, typename... Args>
+        static T& component(entity, Args&&...);
+
+        template<typename T>
+        static T& get(entity);
+
+        template<typename... T>
+        static void remove(entity);
+    private:
+        static shared_ptr<registry> entt_reg;
+    };
+}
